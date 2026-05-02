b436461ffaccb239194c8cc8e8d495a8b6f9e06d
jolethen <155651557+jolethen@users.noreply.github.com>
2026-05-02 19:14:55 +0530

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -4,26 +4,34 @@ minetest.register_chatcommand("spawn_npc", {
     privs = {server = true},
     func = function(name, param)
         local player = minetest.get_player_by_name(name)
-        local args = param:split(" ")
-        if #args < 2 then return false, "Usage: /spawn_npc <name> <role>" end
+        if not player then return false, "Player not found." end
+        
+        local args = string.split(param, " ")
+        local npc_name = args[1] or "Villager"
+        local role = args[2] or "roamer"
         
         local pos = player:get_pos()
+        -- Ensure starting position is clean for the entity
+        pos.y = pos.y + 0.5 
+        
         local npc = minetest.add_entity(pos, "techblox_npcs:npc")
         if npc then
             local ent = npc:get_luaentity()
             ent.npc_id = tostring(math.random(10000, 99999))
-            ent.npc_name = args[1]
-            ent.role = args[2]
+            ent.npc_name = npc_name
+            ent.role = role
             
-            -- Set a default 20x20 roam zone around spawn
+            -- Round off bounds immediately for safety
             ent.zones.roam = {
-                pos1 = vector.add(pos, {x=10, y=10, z=10}),
-                pos2 = vector.subtract(pos, {x=10, y=10, z=10})
+                pos1 = vector.add(pos, {x=10, y=5, z=10}),
+                pos2 = vector.subtract(pos, {x=10, y=5, z=10})
             }
             
             table.insert(techblox_npcs.spawned_npcs, {id = ent.npc_id, pos = pos})
             techblox_npcs.save_npc_data()
-            return true, "Spawned " .. args[1] .. " as " .. args[2]
+            return true, "Spawned " .. npc_name .. " as " .. role
+        else
+            return false, "Failed to spawn entity."
         end
     end
 })
